76cec3bd094d5e0e14b247a26478c5c93b1545cb
flavio.santana <9090flavio@gmail.com>
2022-10-13 17:49:51 -0300

    fix: testando um método que lança exception

--- src/main/java/br/com/alura/tdd/service/BonusService.java
+++ src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -8,9 +8,11 @@ public class BonusService {
 
 	public BigDecimal calcularBonus(Funcionario funcionario) {
 		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+
 		if (valor.compareTo(new BigDecimal("1000")) > 0) {
-			valor = BigDecimal.ZERO;
+			throw new IllegalArgumentException("Funcionadio não pode receber bônus!");
 		}
+
 		return valor.setScale(2);
 	}
 
