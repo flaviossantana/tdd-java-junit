021c11c60f05e994f777d75c9e40f4aa58438504
flavio.santana <9090flavio@gmail.com>
2022-10-13 16:31:38 -0300

    feat: testando uma funcionalidade

--- src/test/java/br/com/alura/tdd/modelo/FuncionarioTest.java
+++ src/test/java/br/com/alura/tdd/modelo/FuncionarioTest.java
@@ -1,7 +1,21 @@
 package br.com.alura.tdd.modelo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FuncionarioTest {
+
+    @Test
+    void criarFuncionarioPeloContrutor() {
+        Funcionario funcionario = new Funcionario("Rodrigo", LocalDate.now(), BigDecimal.ZERO);
+        assertEquals("Rodrigo", funcionario.getNome());
+        assertEquals(LocalDate.now(), funcionario.getDataAdmissao());
+        assertEquals(BigDecimal.ZERO, funcionario.getSalario());
+    }
+
 
 }
